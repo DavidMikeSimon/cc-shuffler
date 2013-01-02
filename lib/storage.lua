e5714b0b45912983bfed4e136614c1fe9385cf4b
David Mike Simon <david.mike.simon@gmail.com>
2013-01-01 19:42:15 -0500

    First stab at serialization

---
+++ lib/storage.lua
@@ -0,0 +1,28 @@
+local P = {}
+storage = P
+
+local type = type
+local string = string
+local error = error
+
+setfenv(1, P)
+
+function serialize(fh, o)
+  if type(o) == "number" then
+    fh.write(o)
+  elseif type(o) == "nil" or type(o) == "boolean" then
+    fh.write(tostring(o))
+  elseif type(o) == "string" then
+    fh.write(string.format("%q", o))
+  elseif type(o) == "table" then
+    fh.write("{\n")
+    for k,v in pairs(o) do
+      fh.write("  ", k, " = ")
+      serialize(v)
+      fh.write(",\n")
+    end
+    fh.write("}\n")
+  else
+    error("cannot serialize a " .. type(o))
+  end
+end
